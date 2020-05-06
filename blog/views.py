744a0d700d89f4dbe19fed28187160c977240e98
Chaobo Li <tiwave.lee@hotmail.com>
2020-05-05 20:58:08 -0400

    add session support in index view

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,38 @@ from django.http import HttpResponse,HttpResponseRedirect,Http404
 from blog.models import Category,Article
 from blog.forms import CategoryForm,ArticleForm
 from blog.forms import UserForm,UserProfileForm
+from datetime import datetime
 
 def index(request):
+
     context = {'boldmessage': "товарищ"}
     category_list = Category.objects.order_by('-name')[:5]
     context['categories'] = category_list
     article_list = Article.objects
     context['articles'] = article_list
-    return render(request, 'blog/index.html',context)
+
+    # cookie of visiting
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7],"%Y-%m-%d %H:%M:%S")
+        if (datetime.now()-last_visit_time).days > 0:
+            visits += 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context['visit'] = visits
+
+    response = render(request, 'blog/index.html',context)
+    return response
 
 def category(request, category_slug):
     context = {}
@@ -35,7 +59,8 @@ def article(request, article_slug):
 
     article = get_object_or_404(Article,slug=article_slug)
     context['article'] = article
-
+    article.read += 1
+    article.save()
     return render(request, 'blog/article.html', context)
 
 def about(request):
