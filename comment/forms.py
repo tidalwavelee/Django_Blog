15f1b4a52b73f648ab4450e1f9c2913fd07304b4
Chaobo Li <tiwave.lee@hotmail.com>
2020-12-13 18:31:46 -0500

    hide form label

--- comment/forms.py
+++ comment/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from comment.models import Comment
 
 class CommentForm(forms.ModelForm):
@@ -9,4 +10,7 @@ class CommentForm(forms.ModelForm):
         "body": forms.Textarea(attrs={'cols': 60, 'rows': 6, 'placeholder': "请评论"}),
         "content_type": forms.HiddenInput(),
         "object_id": forms.HiddenInput(),
+      }
+    labels = {
+        "body": _(""),
       }
